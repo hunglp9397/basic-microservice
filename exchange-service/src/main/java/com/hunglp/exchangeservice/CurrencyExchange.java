482403c2512e2cb3f62f43e28d2f9ccace1794df
hunglp9397 <lephihung0997@gmail.com>
2022-10-15 14:43:59 +0700

    create all services, regis to eureka

---
+++ exchange-service/src/main/java/com/hunglp/exchangeservice/CurrencyExchange.java
@@ -0,0 +1,35 @@
+package com.hunglp.exchangeservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyExchange {
+
+    @Id
+    private Long id;
+
+    @Column(name = "currency_from")
+    private String from;
+
+    @Column(name = "currency_to")
+    private String to;
+
+    private BigDecimal conversionMultiple;
+
+    private String environment;
+
+}
+
---
+++ exchange-service/src/main/java/com/hunglp/exchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,38 @@
+package com.hunglp.exchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.env.Environment;
+
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private CurrencyExchangeRepository repository;
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to) {
+        CurrencyExchange currencyExchange
+                = repository.findByFromAndTo(from, to);
+
+        if(currencyExchange ==null) {
+            throw new RuntimeException
+                    ("Unable to Find data for " + from + " to " + to);
+        }
+
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+
+        return currencyExchange;
+
+    }
+
+}

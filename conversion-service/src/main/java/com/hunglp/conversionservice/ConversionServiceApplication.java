0968075e3970c1b97b8116a9c60bf3f0290d7531
hunglp9397 <lephihung0997@gmail.com>
2022-10-15 14:54:36 +0700

    use feign client to call exchange service

--- conversion-service/src/main/java/com/hunglp/conversionservice/ConversionServiceApplication.java
+++ conversion-service/src/main/java/com/hunglp/conversionservice/ConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.hunglp.conversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ConversionServiceApplication {
 
     public static void main(String[] args) {
--- conversion-service/src/main/java/com/hunglp/conversionservice/CurrencyConversionController.java
+++ conversion-service/src/main/java/com/hunglp/conversionservice/CurrencyConversionController.java
@@ -1,6 +1,7 @@
 package com.hunglp.conversionservice;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,9 @@ import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
+
+    @Autowired
+    private CurrencyExchangeProxy proxy;
 
 
 
@@ -38,6 +42,25 @@ public class CurrencyConversionController {
                 currencyConversion.getConversionMultiple(),
                 quantity.multiply(currencyConversion.getConversionMultiple()),
                 currencyConversion.getEnvironment()+ " " + "rest template");
+
+    }
+
+
+    // Use feign client so call currency-exchange service
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ) {
+
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+
+        return new CurrencyConversion(currencyConversion.getId(),
+                from, to, quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + " " + "feign");
 
     }
 
